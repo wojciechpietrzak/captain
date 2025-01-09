bf21e6ef24291df1fb3365da7b968415d9fdd62a
Wojciech <wojciech.pietrzak@7willows.com>
2025-01-09 14:28:27 +0100

    feat: round number control, withdrawal handle

--- src/pairing_engine/server/server.go
+++ src/pairing_engine/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	pb "captain/src/pairing_engine" // Import the generated Protobuf package
 	"context"
+	"fmt"
+	"log"
 )
 
 type PairingEngineServer struct {
@@ -11,7 +13,39 @@ type PairingEngineServer struct {
 
 // Implement the CalculatePairing RPC
 func (s *PairingEngineServer) CalculatePairing(ctx context.Context, req *pb.CalculatePairingRequest) (*pb.CalculatePairingResponse, error) {
-	// Logic for pairing goes here
+	allRounds := int(req.Tournament.AllRoundsNo)
+	currentRound := len(req.Tournament.Rounds) + 1
+
+	if currentRound > allRounds {
+		log.Println("All rounds already paired")
+		return nil, fmt.Errorf("all rounds already paired")
+	}
+
+	applyLast := currentRound == allRounds
+
+	log.Printf("Pairing round %v of %v.", currentRound, allRounds)
+	if applyLast {
+		log.Println("Applying last round special rules.")
+	}
+
+	var currentRoundPlayers []*pb.Player
+	var withdrawn bool
+
+	for _, player := range req.Tournament.Players {
+		withdrawn = false
+		for _, withdrawal := range player.Withdrawals {
+			if (int)(withdrawal.RoundNo) == currentRound {
+				withdrawn = true
+				break
+			}
+		}
+		if !withdrawn {
+			currentRoundPlayers = append(currentRoundPlayers, player)
+		}
+	}
+
+	log.Printf("Current round players: %v", currentRoundPlayers)
+
 	return &pb.CalculatePairingResponse{
 		Pairing: &pb.Pairing{
 			Tables:      []*pb.Table{},      // TODO: implement me
